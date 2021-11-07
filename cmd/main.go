d90ace6ff78694fb0c7356f38c47657ec3ea1fc8
JleenDeniel <d.zhilin2013@yandex.ru>
2021-11-07 15:34:39 +0300

    add proto

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,5 @@
-package main 
+package main
 
-import (
-	"net/http"
-	"log"
-)
+func main() {
 
-func main(){
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	log.Println("Запуск веб-сервера на http://127.0.0.1:3000")
-	err := http.ListenAndServe(":3000", mux)
-	log.Fatal(err)
-}
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Привет!"))
 }
